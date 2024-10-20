6e58da88d2af2cc71b2d42e57dbbd5020d837413
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-10-20 18:51:55 -0300

    add URP, animation

--- Basics/2- Building a Graph/Assets/Scripts/Graph.cs
+++ Basics/2- Building a Graph/Assets/Scripts/Graph.cs
@@ -5,20 +5,22 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
+    Transform[] points;
+
 	[SerializeField, Range(10, 100)]
 	int resolution = 10;
     // Start is called before the first frame update
     void Awake()
     {
+        points = new Transform[resolution];
 		var position = Vector3.zero;
         float step = 2f / resolution;
 		var scale = Vector3.one * step;
 
-        for (int i = 0; i < resolution; i++)
+        for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step / - 1f;
-			position.y = position.x * position.x * position.x;
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
 			point.localPosition = position;
 			point.localScale = scale;
             point.SetParent(transform, false);
@@ -28,6 +30,13 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+			position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
         
     }
 }
